4eed6d0b4c0dec34aaa8e36c2b7f48b8ba787879
skoda888 <robertalexandru2712@gmail.com>
2020-08-27 18:02:53 +0300

    User fields validations all right.

--- server/src/database-models/users-db.ts
+++ server/src/database-models/users-db.ts
@@ -6,10 +6,31 @@ export const databaseUrl = 'mongodb://127.0.0.1/users';
 
 // User's collectection schema
 const schema = new mongoose.Schema({
-    fullName: { type: String, required: true },
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, min: 8, required: true }
+    fullName: { 
+        type: String, 
+        required: true 
+    },
+    username: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true,
+        validate: {
+            validator: function(value: string): boolean {
+                let regex = /^[a-zA-Z0-9_]+\@[a-zA-Z0-9_]+\.com$/
+                return regex.test(value);
+            }
+        } 
+    },
+    password: { 
+        type: String, 
+        min: 8, 
+        required: true 
+    }
 });   
 
 // User's collection model
